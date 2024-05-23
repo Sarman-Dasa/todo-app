812f15634d3f68d7f46d7874f04455f118ae8e66
Sarman-Dasa <sarmand@zignuts.com>
2024-05-23 15:44:47 +0530

    install sweetalert packge & add deleteConfirmation alert message

--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -3,19 +3,46 @@ import { useSelector, useDispatch } from "react-redux";
 import "../css/todo.css";
 import { deleteTodo } from "../app/todo/todosSlice";
 import UpdatedTodo from "./UpdateTodo";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
 export default function TodoList() {
   const todoList = useSelector((state) => state.todos.todos);
 
   const dispatch = useDispatch();
   const [isEdit, setIsEdit] = useState(false);
   const [editItem, setEditItem] = useState();
-  const deleteitem = (id) => {
+
+  const deleteItem = (id) => {
     dispatch(deleteTodo(id));
+    MySwal.fire("Deleted!", "", "success");
   };
 
+  // Open modal for todo data edit
   const openEditModal = (item) => {
     setEditItem(item);
     setIsEdit(true);
+  };
+
+  // Show Confirmation message for todo item delete
+  const deleteConfirmation = (id) => {
+    MySwal.fire({
+      title: "Do you want to delete this item?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Delete",
+      confirmButtonClass: "",
+      customClass: {
+        confirmButton: "btn text-bg-danger",
+        cancelButton: "btn btn-danger",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteItem(id); // Yes to call deleteItem function
+      }
+    });
   };
 
   if (!todoList) {
@@ -35,7 +62,7 @@ export default function TodoList() {
                 <span className="action">
                   <button
                     className="deleteBtn"
-                    onClick={() => deleteitem(item.id)}
+                    onClick={() => deleteConfirmation(item.id)}
                   >
                     Delete
                   </button>
